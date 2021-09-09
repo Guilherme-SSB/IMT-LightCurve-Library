21c20f24f383f29d0b5f61d01a9320956bd10dd8
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-09-09 10:04:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='imt_lightcurve',
-    python_requires='>=3.8.7',
+    ython_requires='>=3.7.11',
     packages=['imt_lightcurve', 'imt_lightcurve.models', 'imt_lightcurve.help_functions', 'imt_lightcurve.simulation', 'imt_lightcurve.visualization'],
     version='1.1',
     description='Library created to manipulate LightCurves',
