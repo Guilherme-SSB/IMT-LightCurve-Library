043128711ca3af3cf16511b0c2552cfa07e287d4
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-10-10 22:40:30 -0300

    Update test-simulation-v2.py

--- test-simulation-v2.py
+++ test-simulation-v2.py
@@ -5,22 +5,27 @@ import numpy as np
 
 from imt_lightcurve.models.lightcurve import LightCurve
 from imt_lightcurve.simulation.simulation import Simulate
+from imt_lightcurve.visualization.data_viz import multi_line_plot
 
 # Chosen a LightCurve to simulation process
-LIGHTCURVE = 'RESAMPLED_0315239728_20100305T001525'
+LIGHTCURVE = 'RESAMPLED_0101086161_20070516T060226'
 
 
 # Importing lightcurve data from github
-data = pd.read_csv(
-    'https://raw.githubusercontent.com/Guilherme-SSB/IC-CoRoT_Kepler/main/resampled_files/' + LIGHTCURVE + '.csv')
+data = pd.read_csv('https://raw.githubusercontent.com/Guilherme-SSB/IC-CoRoT_Kepler/main/resampled_files/' + LIGHTCURVE + '.csv')
 time = data.DATE.to_numpy()
 flux = data.WHITEFLUX.to_numpy()
 curve = LightCurve(time, flux)
+curve.plot()
 
 # Folding lightcurve
-folded_curve = curve.fold(window_filter=21, order_filter=3)
-# print(folded_curve)
-folded_curve.plot()
+folded_curve = curve.fold(window=0.15, smooth_curve=False)
+# folded_curve.plot()
+folded_curve_smooth = curve.fold(window=0.15, smooth_curve=True)
+
+## Ploting results
+multi_line_plot(folded_curve.time, folded_curve.flux, folded_curve_smooth.flux)
+
 
 
 # %%
@@ -30,36 +35,37 @@ folded_curve.plot()
 curve_id = int(LIGHTCURVE.split('_')[1][1:])
 print(f'CoRoT-ID: {curve_id} real parameters:\n')
 
-# Transit impact parameter
-b_real = LightCurve.get_true_value(curve_id, 'b')
+# Orbital period values to be considered
+period_real = LightCurve.get_true_value(curve_id, 'Per')
 
 # Radius values of the planet compared to the star
 p_real = LightCurve.get_true_value(curve_id, 'Rp/R*')
 
-# Orbital period values to be considered
-period_real = LightCurve.get_true_value(curve_id, 'Per')
-
 # Orbital radius values compared to star radius
 adivR_real = LightCurve.get_true_value(curve_id, 'a/R*')
 
+# Transit impact parameter
+b_real = LightCurve.get_true_value(curve_id, 'b')
+
 
 # Defining grid of parameters to search
-b_values = LightCurve.define_interval_b(b_real)
-print('b_impact =', round(b_real, 2))
-print('b impact grid:', b_values, end='\n\n')
+period_values = LightCurve.define_interval_period(period_real)
+print('period =', round(period_real, 2))
+# print('period grid:', period_values, end='\n\n')
 
 p_values = LightCurve.define_interval_p(p_real)
 print('p =', round(p_real, 2))
-print('p grid:', p_values, end='\n\n')
-
-period_values = LightCurve.define_interval_period(period_real)
-print('period =', round(period_real, 2))
-print('period grid:', period_values, end='\n\n')
-
+# print('p grid:', p_values, end='\n\n')
 
 adivR_values = LightCurve.define_interval_adivR(adivR_real)
 print('adivR =', round(adivR_real, 2))
-print('adivR grid:', adivR_values, end='\n\n')
+# print('adivR grid:', adivR_values, end='\n\n')
+
+b_values = LightCurve.define_interval_b(b_real)
+print('b_impact =', round(b_real, 2))
+# print('b impact grid:', b_values, end='\n\n')
+
+
 
 
 # %%
