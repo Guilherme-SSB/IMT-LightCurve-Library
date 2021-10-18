dfda17eba342749197ad45dba328936df8a6978e
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-10-18 07:12:18 -0300

    Update test-simulation-final.py

--- test-simulation-final.py
+++ test-simulation-final.py
@@ -75,6 +75,7 @@ import os
 import pandas as pd
 import numpy as np
 from imt_lightcurve.models.lightcurve import LightCurve
+from imt_lightcurve.simulation.simulation import Simulate
 
 # FILTERS_RESULTS_PATH = 'C:/Users/guisa/Google Drive/01 - Iniciação Científica/02 - Datasets/exoplanets_confirmed/filters'
 FILTERS_RESULTS_PATH = "C:/Users/guisa/Desktop/filters"
@@ -84,7 +85,7 @@ cutoff_freqs = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 FINAL_PATH = ''
 
 filter_technique = 'butterworth'
-
+final_table_results = pd.DataFrame()
 
 ## Definindo paths para cada sub-pasta
 if filter_technique.upper() == 'BUTTERWORTH':
@@ -98,20 +99,68 @@ if filter_technique.upper() == 'BUTTERWORTH':
             for root_dir_path, sub_dirs, files in os.walk(FINAL_PATH):
                 for i in range(0, len(files)):
                     if files[i].endswith('.csv'):
+
+                        ## Loading folded curve
                         path = root_dir_path + '/' + files[i]
-                        curve_id = path.split('/')[-1].split('_')[-2][1:]
+                        curve_id = int(path.split('/')[-1].split('_')[-2][1:].strip())
                         data = pd.read_csv(path)
                         time = data.DATE.to_numpy()
                         flux = data.WHITEFLUX.to_numpy()
-
                         curve = LightCurve(time, flux)
                         error = np.std(curve.flux)
                         error_array = [error for i in range(len(curve.flux))]
                         curve.flux_error = error_array
 
-                        curve.plot(title=f'CoRoT ID: {curve_id}, {filter_technique.capitalize()} Order: {order} Cutoff Freq: {cutoff_freq}')
+                        # curve.plot(title=f'CoRoT ID: {curve_id}, {filter_technique.capitalize()} Order: {order} Cutoff Freq: {cutoff_freq}')
 
-                    break
+                        ## Extracting lightcurves real parameters
+                        # Orbital period values to be considered
+                        period_real = LightCurve.get_true_value(curve_id, 'Per')
+
+                        # Radius values of the planet compared to the star
+                        p_real = LightCurve.get_true_value(curve_id, 'Rp/R*')
+
+                        # Orbital radius values compared to star radius
+                        adivR_real = LightCurve.get_true_value(curve_id, 'a/R*')
+
+                        # Transit impact parameter
+                        b_real = LightCurve.get_true_value(curve_id, 'b')
+
+                        ## Defining grid of parameters to search
+                        period_values = LightCurve.define_interval_period(period_real)
+                        p_values = LightCurve.define_interval_p(p_real)
+                        adivR_values = LightCurve.define_interval_adivR(adivR_real)
+                        b_values = LightCurve.define_interval_b(b_real)
+
+
+                        ## Start the best parameters search
+                        SimulationObject = Simulate()
+
+                        try: 
+                            results = SimulationObject.simulate_values(
+                                CoRoT_ID=curve_id, observed_curve=curve,
+                                b_values=b_values,
+                                p_values=p_values,
+                                period_values=period_values,
+                                adivR_values=adivR_values,
+                                set_best_values=True,
+                                results_to_csv=False,
+                                filter_technique=f'{filter_technique}',
+                                filter_order=f'{order}',
+                                filter_cutoff=f'{cutoff_freq}'
+                            )
+
+                            final_table_results = final_table_results.append(results)
+                        
+                        except:
+                            # Se der problema, eu posso salvar os resultados e ja era
+                            final_table_results.head()
+                            raise ValueError()
+                            
+                        
+
+
+                    final_table_results.head()
                 break
             break
         break
@@ -127,5 +176,23 @@ if filter_technique.upper() == 'BUTTERWORTH':
 
 
 
+
+# %%
+import pandas as pd
+import numpy as np
+from imt_lightcurve.models.lightcurve import LightCurve
+
+LIGHTCURVE = 'RESAMPLED_0315211361_20100305T001525'
+curve_id = int(LIGHTCURVE.split('_')[1][1:])
+
+data = pd.read_csv('https://raw.githubusercontent.com/Guilherme-SSB/IC-CoRoT_Kepler/main/resampled_files/' + LIGHTCURVE + '.csv')
+flux = data.WHITEFLUX.to_numpy()
+flux = flux / np.median(flux)
+
+time = data.DATE.to_numpy()
+
+curve = LightCurve(time=time, flux=flux)
+curve.plot()
+curve.fold(corot_id=curve_id).plot(title=f'Folded LC of CoRoT-ID: {curve_id}')
 
 # %%
