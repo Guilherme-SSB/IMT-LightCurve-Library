a210edb8f20e419e7162fb6058b48eb1ef0eff65
Guilherme Samuel de Souza Barbosa <gui.samuel10@gmail.com>
2021-10-25 22:24:20 -0300

    Update lightcurve.py

--- imt_lightcurve/models/lightcurve.py
+++ imt_lightcurve/models/lightcurve.py
@@ -184,128 +184,152 @@ class LightCurve(BaseLightCurve):
     def median_filter(self, numNei, numExpansion: int = 70):
         return self.__apply_filter(self.time, self.flux, filter_technique='median', numNei=numNei, numExpansion=numExpansion, cutoff_freq=None, order=None)
 
-    def fold(self, corot_id: int):
-        P = LightCurve.get_true_value(corot_id, 'Per')
-        time = np.arange(0, len(self.time))
-        normalized_flux = self.flux / np.median(self.flux)
-        normalized_inverted_curve = LightCurve(
-            time=time, flux=-1*normalized_flux)
-        x = normalized_inverted_curve.flux
+    def fold(self, corot_id: str):
+        """codigo velho"""
+        # P = LightCurve.get_true_value(corot_id, 'Per')
+        # time = np.arange(0, len(self.time))
+        # normalized_flux = self.flux / np.median(self.flux)
+        # normalized_inverted_curve = LightCurve(
+        #     time=time, flux=-1*normalized_flux)
+        # x = normalized_inverted_curve.flux
 
-        # Determine eclipses peaks
-        peaks, _ = find_peaks(x, distance=P*100)
-        peaks = peaks[1:-1]
-        totalEclipses = len(peaks)
+        # # Determine eclipses peaks
+        # peaks, _ = find_peaks(x, distance=P*100)
+        # peaks = peaks[1:-1]
+        # totalEclipses = len(peaks)
 
-        # Determine eclipses widths
-        widths = peak_widths(x, peaks, rel_height=0.5)
-        eclipse_widths_mean = np.mean(widths[0])
+        # # Determine eclipses widths
+        # widths = peak_widths(x, peaks, rel_height=0.5)
+        # eclipse_widths_mean = np.mean(widths[0])
+
+        # Summing all eclipses
+        # sum_eclipses_flux = 0
+        # for numEclipse in range(totalEclipses):
+        #     cond = (normalized_inverted_curve.time > (peaks[numEclipse]-eclipse_widths_mean/2)) & (
+        #         normalized_inverted_curve.time < (peaks[numEclipse]+eclipse_widths_mean/2))
+        #     sum_eclipses_flux += normalized_inverted_curve.flux[cond]
+
+        # sum_eclipses_flux *= -1
+
+        # # Computing the average eclipse
+        # avg_eclipses_flux = sum_eclipses_flux/totalEclipses
+
+        # time_fold = np.arange(-len(avg_eclipses_flux)/2,
+        #                       len(avg_eclipses_flux)/2)
+
+        # # Return
+        # phase_folded_curve = PhaseFoldedLightCurve(
+        #     time=time_fold, flux=avg_eclipses_flux)
+        # error = np.std(avg_eclipses_flux)
+        # error_array = [error for i in range(len(avg_eclipses_flux))]
+        # phase_folded_curve.flux_error = error_array
+
+        # return phase_folded_curve
+
+        """codigo novo"""
+        # Ajusts time axis
+        time = np.arange(0, len(self.flux))
+
+        # Based on corot_id, load the positions and the widths of eclipses
+        positions, width = self.__load_fold_infos(corot_id=corot_id)
+
+        # How many eclipses there are on this curve?
+        totalEclipses = len(positions)
 
         # Summing all eclipses
         sum_eclipses_flux = 0
-        for numEclipse in range(totalEclipses):
-            cond = (normalized_inverted_curve.time > (peaks[numEclipse]-eclipse_widths_mean/2)) & (
-                normalized_inverted_curve.time < (peaks[numEclipse]+eclipse_widths_mean/2))
-            sum_eclipses_flux += normalized_inverted_curve.flux[cond]
-
-        sum_eclipses_flux *= -1
+        for eclipse in range(totalEclipses):
+            cond = (time > (positions[eclipse]-width/2)) & (time < (positions[eclipse]+width/2))
+            sum_eclipses_flux += self.flux[cond]
 
         # Computing the average eclipse
         avg_eclipses_flux = sum_eclipses_flux/totalEclipses
 
-        time_fold = np.arange(-len(avg_eclipses_flux)/2,
-                              len(avg_eclipses_flux)/2)
-
         # Return
-        phase_folded_curve = PhaseFoldedLightCurve(
-            time=time_fold, flux=avg_eclipses_flux)
-        error = np.std(avg_eclipses_flux)
+        time_fold = np.arange(-len(avg_eclipses_flux)/2, +len(avg_eclipses_flux)/2)
+
+        folded_curve = PhaseFoldedLightCurve(time=time_fold, flux=avg_eclipses_flux)
+        error = np.std(avg_eclipses_flux) #TODO Revisar esse erro aqui !!
         error_array = [error for i in range(len(avg_eclipses_flux))]
-        phase_folded_curve.flux_error = error_array
+        folded_curve.flux_error = error_array
 
-        return phase_folded_curve
-
-    def __load_fold_infos(self, corot_id):
-
+        return folded_curve
+        
+    def __load_fold_infos(self, corot_id: str):
         if corot_id == '100725706':
             eclipses_position = [1072, 2471, 3871, 5275, 6679, 8081, 9482, 10888, 12291, 13693]
-            eclipses_width = 13
+            eclipses_width = 14
 
         if corot_id == '101086161':
             eclipses_position = [244, 899, 1554, 2217, 2871, 3532, 4188, 4848, 5505, 6161, 6820, 7480, 8134, 8792, 9455, 10109, 10771, 11428, 12088, 12745, 13402, 14061, 14719]
             eclipses_width = 13
 
         if corot_id == '101368192':
-            pass
+            eclipses_position = [377, 797, 1220, 1641, 2067, 2486, 2903, 3328, 3747, 4171, 4594, 5012, 5436, 5859, 6274, 6698, 7116, 7542, 7961, 8380, 8805, 9226, 9648, 10068, 10488, 10910, 11334, 11754, 12176, 12602, 13024, 13437, 13862, 14287, 14704]
+            eclipses_width = 17
 
         if corot_id == '102671819':
-            pass
-
-        if corot_id == '102725122':
-            pass
+            eclipses_position = [135, 461, 783, 1106, 1431, 1753, 2078, 2401, 2729, 3049, 3371, 3694, 4019, 4345, 4669, 4989, 5316, 5636, 5963, 6285, 6611, 6933, 7256, 7581, 7906, 8226, 8553, 8875, 9199, 9524, 9846, 10171, 10493, 10819, 11145, 11465, 11790, 12113, 12438, 12763, 13088, 13409, 13735, 14059, 14385, 14757, 15031]
+            eclipses_width = 14
 
         if corot_id == '102764809':
-            pass
+            eclipses_position = [324, 800, 1278, 1754, 2227, 2702, 3180, 3651, 4128, 4604, 5074, 5555, 6028, 6504, 6981, 7455, 7930, 8408, 8882, 9356, 9835, 10355, 10831, 11306, 11782, 12254, 12730, 13207, 13685, 14159, 14632]
+            eclipses_width = 16
 
         if corot_id == '102890318':
-            pass
+            eclipses_position = [69, 485, 898, 1313, 1731, 2144, 2561, 2974, 3392, 3806, 4219, 4637, 5050, 5471, 5882, 6290, 6712, 7120, 7538, 7955, 8368, 8785, 9205, 9618, 10031, 10445, 10862, 11274, 11691, 12108, 12522, 12935, 13350, 13760, 14184, 14598, 15008]
+            eclipses_width = 31
 
         if corot_id == '102912369':
-            pass
+            eclipses_position = [1651, 4051, 6441, 8836, 11246, 13644]
+            eclipses_width = 50
 
         if corot_id == '105209106':
-            pass
-
-        if corot_id == '105228856':
-            pass
+            eclipses_position = [112, 558, 996, 1425, 1863, 2300, 2738, 3168, 3615, 4037, 4481, 4918, 5359, 5795, 6224, 6658, 7094, 7530, 7975, 8408, 8846, 9289, 9714, 10154, 10590, 11029, 11464, 11901, 12770, 13209, 13645, 14086, 14517, 14965]
+            eclipses_width = 20
 
         if corot_id == '105793995':
-            pass
-
+            eclipses_position = [390, 896, 1402, 1908, 2405, 2906, 3413, 3911, 4415, 4920, 5412, 5917, 6421, 6926, 7433, 7939, 8444, 8952, 9457, 9964, 10466, 10973, 11480, 11985, 12492, 13081, 14010, 14516, 15022]
+            eclipses_width = 8
+            
         if corot_id == '105819653':
-            pass
-
+            eclipses_position = [133, 1054, 1845, 2684, 3701, 4709, 5705, 6726, 7736, 8990, 9774, 10785, 11801, 12814, 13836, 14840]
+            eclipses_width = 20
+        
         if corot_id == '105833549':
-            pass
-
+            eclipses_position = [131, 443, 753, 1066, 1377, 1686, 1997, 2301, 2615, 2924, 3237, 3546, 3849, 4160, 4469, 4783, 5095, 5389, 5702, 6013, 6324, 6635, 6948, 7260, 7569, 7880, 8194, 8505, 8816, 9127, 9440, 9751, 10065, 10375, 10686, 10999, 11312, 11623, 11935, 12245, 12555, 12868, 13181, 13489, 13802, 14115, 14427, 14737, 15049]
+            eclipses_width = 12
+        
         if corot_id == '105891283':
-            pass
-
+            eclipses_position = [3202, 13087]
+            eclipses_width = 48
+        
         if corot_id == '106017681':
-            pass
-
+            eclipses_position = [560, 1485, 2400, 3322, 4237, 5144, 6071, 6994, 7920, 8846, 9767, 10695, 11621, 12544, 13466, 14393]
+            eclipses_width = 19
+        
         if corot_id == '110839339':
-            pass
-
-        if corot_id == '110864907':
-            pass
-
-        if corot_id == '310247220':
-            pass
-
-        if corot_id == '311519570':
-            pass
-
+            eclipses_position = [27, 457, 993, 1516, 2045, 2581, 3108, 3639, 4166, 4429, 4693, 5224, 5754, 6283, 6814, 7347, 7881, 8404, 8935, 9468, 9999, 10523, 11055, 11586, 12117, 12648, 13175, 13706, 14238, 14764]
+            eclipses_width = 17
+        
         if corot_id == '315198039':
-            pass
+            eclipses_position = [507, 2933, 5363, 7741, 10174, 12570, 15045]
+            eclipses_width = 122
 
         if corot_id == '315211361':
-            pass
+            eclipses_position = [251, 1416, 2616, 3798, 4967, 6136, 7331, 8498, 9688, 10840, 12028, 13220, 14384]
+            eclipses_width = 60
 
         if corot_id == '315239728':
-            pass
-
-        if corot_id == '630831435':
-            pass
-
-        if corot_id == '652180928':
-            pass
+            eclipses_position = [3404, 9142, 14857]
+            eclipses_width = 57
 
         if corot_id == '652180991':
-            pass
-        else:
-            raise NameError(
-                f'{corot_id} is not a valid CoRoT-ID. Please check it ')
+            eclipses_position = [852, 1778, 2728, 3661, 4595, 5535, 6478, 7417, 8350, 9294, 10228, 11163, 12103, 13039, 13979, 14915]
+            eclipses_width = 42
+        
+        # else:
+        #     raise NameError(f'{corot_id} is not a valid CoRoT-ID. Please check it')
 
         return eclipses_position, eclipses_width
 
@@ -331,7 +355,6 @@ class LightCurve(BaseLightCurve):
         if any(complete_table_5['CoRoT'] == corot_id) == False:
             raise Error(f'Not found CoRoT-ID: {corot_id} at Table 5')
         else:
-            # return float(complete_table_5[complete_table_5['CoRoT'] == corot_id][parameter].values[0].split('±')[0])
             return float(complete_table_5[complete_table_5['CoRoT'] == corot_id][parameter].to_numpy()[0])
 
     @abstractmethod
@@ -387,7 +410,7 @@ class LightCurve(BaseLightCurve):
 
         DATASET_PATH = WHERE_ARE_THE_RESAMPLED_DATASET
 
-        total = 33
+        total = 18
 
         if cutoff_freq_range != None:
             cutoff_freqs = np.arange(
